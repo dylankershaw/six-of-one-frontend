365c4d30be061eac88edff2d0b8d12c809e1031c
github username <dylankershaw@me.com>
2018-01-27 18:32:20 -0500

    replace cover photo component with image

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import coverPhoto from "../assets/cover_photo.jpg";
 import logo from "../assets/logo_and_hamburger.svg";
 
 class App extends Component {
@@ -11,7 +12,8 @@ class App extends Component {
   render() {
     return (
       <div>
-        <img alt="six of one logo" src={logo} />
+        <img id="logo" alt="six of one logo" src={logo} />
+        <img id="cover-photo" alt="mountain landscape" src={coverPhoto} />
       </div>
     );
   }
